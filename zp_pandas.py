61ccf2296475e6e2aeb9bd447a525e9f8c906082
ANovopoltsev <113423630+ANovopoltsev@users.noreply.github.com>
2023-12-14 15:28:47 +0400

    Add files via upload

--- zp_pandas.py
+++ zp_pandas.py
@@ -12,19 +12,22 @@ download_catalog = [
 
 column_name = {
     1: 'date',
-    2: 'district',
-    3: 'group',
-    4: 'category_of_worker',
-    5: 'salary',
-    6: 'attitude_salary',
-    7: 'average_salary_for_individual_entrepreneus'
+    0: 'district',
+    4: 'group',
+    6: 'category_of_worker',
+    2: 'salary',
+    5: 'attitude_salary',
+    3: 'average_salary_for_individual_entrepreneus'
 }
+
 
 data = pd.read_excel('03-23-01.xlsx', skiprows=6, usecols='A:B,j')
 data.insert(1, 'date', '07.01.2023' ,  False)
 data.columns = range(data.columns.size)
 data = data.rename(columns=column_name)
-df_columns = pd.Series(column_name.items(), name='column_name')
-data = data.join(df_columns)
-
-data
+data = data.rename(columns={0:'district', 2:'salary'} )
+#(100 * data[['salary']] / data.average_salary_for_individual_entrepreneus)
+data['salary']=data['salary'].convert_dtypes(convert_integer=True)
+data['average_salary_for_individual_entrepreneus']=data['average_salary_for_individual_entrepreneus'].convert_dtypes(convert_integer=True)
+print(data['average_salary_for_individual_entrepreneus'])
+data.assign(attitude_salary = data.salary * 100 / data.average_salary_for_individual_entrepreneus)
